bb5526b725d0b0de108cadbbc202e1646838f999
lisamarie616 <lisamarie616@gmail.com>
2016-04-12 13:32:11 -0700

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -7,8 +7,8 @@ angular
           templateUrl: 'views/user.html',
           controller: 'UserController as user',
           resolve: {
-            user: function($http, $route){
-              return $http.get('http://0.0.0.0:8882/rest/user/' + $route.current.params.name);
+            user: function($http, $stateParams){
+              return $http.get('http://0.0.0.0:8882/rest/user/' + $stateParams.name);
             }
           }
         });
