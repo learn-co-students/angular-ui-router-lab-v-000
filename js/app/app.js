84d96ed49f6b4fa0b15726e8db7bbada6213eb31
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-04-17 12:54:48 -0400

    Update route

--- js/app/app.js
+++ js/app/app.js
@@ -2,15 +2,30 @@ angular
     .module('app', ['ui.router'])
     .config(function($stateProvider){
       $stateProvider
-        .state('user.id', {
+        .state('user.', {
           url: '/user/:id',
           templateUrl: 'views/user.html',
           controller: "UserController as user",
           resolve: {
-            user: function($route, $http){
-              debugger;
-              return $http.get('/rest/user/' + $route.current.params.id)
+            user: function($stateParams, $http){
+              return $http.get('http://0.0.0.0:8882/rest/user' + $stateParams.id)
             }
           }
         })
     });
+
+// angular
+//     .module('app', ['ui.router'])
+// 	.config(function ($stateProvider) {
+// 		$stateProvider
+// 			.state('user', {
+// 				url: '/user/:id',
+// 				templateUrl: 'views/user.html',
+// 				controller: 'UserController as user',
+// 				resolve: {
+// 					user: function ($http, $stateParams) {
+// 						return $http.get('http://0.0.0.0:8882/rest/user/' + $stateParams.id);
+// 					}
+// 				}
+// 			});
+// 	});
