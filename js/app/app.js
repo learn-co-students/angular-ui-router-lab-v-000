28300912befd8d66e83aa113ac0a1372d948e4bf
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-04-17 12:46:27 -0400

    Passing tests

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,16 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function($stateProvider){
+      $stateProvider
+        .state('user.id', {
+          url: '/user/:id',
+          templateUrl: 'views/user.html',
+          controller: "UserController as user",
+          resolve: {
+            user: function($route, $http){
+              debugger;
+              return $http.get('/rest/user/' + $route.current.params.id)
+            }
+          }
+        })
+    });
