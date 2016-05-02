a019177bd0e29ed94659226a64086f9a7c51a5a9
NStephenson <nickstephenson90@gmail.com>
2016-05-02 19:14:20 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,14 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function($stateProvider){
+      $stateProvider.state('user', {
+        url: '/user/:name',
+        templateUrl: 'views/user.html',
+        controller: 'UserController as user',
+        resolve: {
+          user: function($http){
+            $http.get('http://0.0.0.0:8882/rest/user/' + $stateParams.name);
+          }
+        }
+      });
+    });
