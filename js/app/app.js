cafbd4a26c34aab36f981fc7d4d5d43a93adece3
Marco Tsui <marcotsui2003@yahoo.com>
2016-05-05 15:36:12 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,15 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function($stateProvider) {
+      $stateProvider
+      .state('user', {
+        url: '/user/:id',
+        templateUrl: 'views/user.html',
+        controller: 'UserController',
+        resolve: {
+          user: function($routeParams, Userservice) {
+            return Userservice.getUser($routeParams.id);
+          }
+        }
+      });
+    });
