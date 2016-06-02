aefee6afbe0ce85953a7fdae1c25da70d6a450e6
authorbeard <msnolan@gmail.com>
2016-06-02 18:37:01 -0400

    specs passing. no idea why. ui-sref not working. does not run in chrome

--- js/app/app.js
+++ js/app/app.js
@@ -2,10 +2,10 @@ angular
     .module('app', ['ui.router'])
     .config(function ($stateProvider){
         $stateProvider
-            .state('user', {
+            .state('user.name', {
                 url: '/user/:id',
                 templateUrl: 'views/NUTS.js',
-                controller: 'UserController.js',
+                controller: 'UserController as user',
                 resolve: {
                     function($http, $route){
                       user=$http.get('http://0.0.0.0:8882/rest/user' + route.current.params.id)
