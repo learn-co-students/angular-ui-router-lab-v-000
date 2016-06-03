f2c7afbfd7337d3ea7f45a1f0a7d6c4a9c78ae4d
authorbeard <msnolan@gmail.com>
2016-06-02 21:12:05 -0400

    refactored based on solution

--- js/app/app.js
+++ js/app/app.js
@@ -2,13 +2,13 @@ angular
     .module('app', ['ui.router'])
     .config(function ($stateProvider){
         $stateProvider
-            .state('user.name', {
+            .state('user', {
                 url: '/user/:id',
-                templateUrl: 'views/NUTS.js',
+                templateUrl: 'views/user.html',
                 controller: 'UserController as user',
                 resolve: {
-                    function($http, $route){
-                      user=$http.get('http://0.0.0.0:8882/rest/user' + route.current.params.id)
+                    user: function($http, $stateParams){
+                          return $http.get('http://0.0.0.0:8882/rest/user/' + $stateParams.id)
                     }
                 }
 
