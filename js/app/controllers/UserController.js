9604ae9089d5c855e1bc861694cfb8237014abc0
Louiefigz <figueroa.aaron@gmail.com>
2016-06-15 15:23:33 -0400

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,6 +1,7 @@
-function UserController() {
-}
+function UserController(user) {
+		this.data = user.data;
+	}
 
-angular
-	.module('app')
-	.controller('UserController', UserController);
+	angular
+		.module('app')
+		.controller('UserController', UserController);
