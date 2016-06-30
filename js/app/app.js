90b9f904322c9e1d71a5897664d1d8eec0c52e98
Tyler Smith <oats000@hotmail.com>
2016-06-30 23:21:03 +0000

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,16 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('user', {
+                url: '/user',
+                templateUrl: 'views/user.html',
+                controller: 'UserController as user'
+                resolve: {
+                  user: function ($http, $route) {
+                    return $http.get('http://0.0.0.0:8882/rest/user/' + $route.current.params.id);
+                  }
+                }
+            });
+    });
+// test 
