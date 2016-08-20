d1153b2289b9fcb973215a60794489559b82e0e8
Peter Jacobson <github email address>
2016-08-20 11:58:23 -0400

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,10 +1,15 @@
 angular
-    .module('app', ['uiRouter']);
+    .module('app', ['ui.router']);
     .config(function ($stateProvider) {
 $stateProvider
 .state('user', {
 url: '/user/:id',
 templateUrl: 'views/user.html',
-controller: 'UserController'
+controller: 'UserController as user',
+resolve: {
+ user: function ($http, $stateParams) {
+             return $http.get('http://0.0.0.0:8882/rest/user/' + $stateParams.id)
+           }
+}
 });
 });
