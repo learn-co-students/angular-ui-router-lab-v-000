fa54b18ec93286da998e1805265182965b964441
kaileighrose <kaileighrose@gmail.com>
2016-11-11 17:16:25 -0500

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,15 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('user', {
+                url: '/user/:id',
+                templateUrl: 'views/user.html',
+                controller: 'UserController',
+                resolve: {
+                  user: function ($http, $state) {
+            return $http.get('http://0.0.0.0:8882/rest/user/' + $state.current.params.id);
+          }
+                }
+            });
+    });
