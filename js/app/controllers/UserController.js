3486de1bcac28da77ee8d124db4fd339d69a72b8
Dom McKellar <dom.mckellar@gmail.com>
2016-12-04 12:38:11 -0800

    Done.

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,4 +1,13 @@
-function UserController() {
+// function UserController(user) {
+//   debugger
+//   this.user = user.data;
+// }
+//
+// angular
+// 	.module('app')
+// 	.controller('UserController', UserController);
+function UserController(user) {
+	this.data = user.data;
 }
 
 angular
