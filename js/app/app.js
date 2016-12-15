1a258da1a0c55f459f7103035a22beb6549dbde1
lenn hypolite <lennhydesign@gmail.com>
2016-12-15 02:15:22 -0500

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,15 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('user', {
+                url: '/user/:id',
+                templateUrl: 'views/user.html',
+                controller: 'UserController',
+                resolve: {
+          					user: function ($http, $route) {
+          						return $http.get('http://0.0.0.0:8882/rest/user/' + $route.current.params.id);
+  					        }
+				        }
+            });
+    });
--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,4 +1,5 @@
 function UserController() {
+	
 }
 
 angular
