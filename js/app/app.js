8dee039f6dbfbc67bea1a4881621c0410cd845f8
Steve Frost <ffrostt@gmail.com>
2016-12-18 19:25:29 -0500

    used ui-router to create states & linked navigation to those states

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,14 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function ($stateProvider) {
+      $stateProvider.state('user', {
+        url: '/user/:id',
+        templateUrl: 'views/user.html',
+        controller: 'UserController as ctrl',
+        resolve: {
+          user: function($http, $stateParams) {
+            return $http.get("http://localhost:8882/rest/user/" + $stateParams.id);
+          }
+        }
+      });
+    });
