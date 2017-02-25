b65e479504798c8d19e02e439303e9c840f75e7f
tttra <tothutra@gmail.com>
2017-02-25 15:40:52 -0500

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,16 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function ($stateProvider) {
+      $stateProvider 
+        .state('user', {
+          url: '/user/:id',
+          templateUrl: 'views/user.html',
+          controller: 'UserController as vm',
+          resolve: {
+            user: function ($routeParams, $http) {
+              return $http.get('http://0.0.0.0:8882/rest/user/' + $routeParams.id)
+            }
+          }
+        })
+    }) 
+;
